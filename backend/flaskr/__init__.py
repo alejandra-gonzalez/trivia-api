14cc72aaa73411c65578a90c5e59624f67725bf7
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2021-04-26 14:35:15 -0700

    delete question by using the question's id completed

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -68,13 +68,29 @@ def create_app(test_config=None):
       'categories': {category.id: category.type for category in categories},
       'current_category': None
       })
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+  # This endpoint deletes a question using a question ID. 
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id==question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+      
+      question.delete()
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+
+      return jsonify({
+        'success': True,
+        'deleted': question_id,
+        'questions': current_questions,
+        'total_questions': len(Question.query.all())
+      })
+    
+    except:
+      abort(422)
 
   '''
   @TODO: 
